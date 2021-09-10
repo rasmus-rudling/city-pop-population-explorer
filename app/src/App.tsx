26b1f640db8115cef516148f9274300da6f10867
Rasmus Rudling <rasmusrudling@gmail.com>
2021-09-10 17:04:39 +0200

    Added react-router-dom & tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
---
+++ app/src/components/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+	return (
+		<div className="flex items-center justify-center min-w-screen min-h-screen">
+			<div className="flex flex-col items-center w-full h-full">
+				<h1 className="text-3xl font-light">CityPop</h1>
+				<p>
+					Discover the number of residents in the city of your choice!
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
